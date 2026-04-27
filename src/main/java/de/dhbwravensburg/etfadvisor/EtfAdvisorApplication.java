0304a158f7c4c95315464f9dd7bd4c3e9b1bd97c
NilsPO <nils.pohlers.np@gmail.com>
2026-04-27 14:42:58 +0200

    feat: initial spring boot project setup

---
+++ src/main/java/de/dhbwravensburg/etfadvisor/EtfAdvisorApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.etfadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtfAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtfAdvisorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dhbwravensburg/etfadvisor/EtfAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.etfadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtfAdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
