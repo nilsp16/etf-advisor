c5fc9b0abd6bc0bd1e18c3e27a28b3307f540227
NilsPO <nils.pohlers.np@gmail.com>
2026-05-20 10:36:03 +0200

    Fix DataSeeder to use null IDs for auto-generation

--- src/main/java/de/dhbwravensburg/etfadvisor/config/DataSeeder.java
+++ src/main/java/de/dhbwravensburg/etfadvisor/config/DataSeeder.java
@@ -19,22 +19,22 @@ public class DataSeeder {
                     "VWRL", "USD", 0.22, 112.45,
                     "Distributing", 89.10, 115.30, 18.5, "Physical","sell")
             );
-            repository.save(new Etf(2L, "iShares Core MSCI World", "IE00B4L5Y983",
+            repository.save(new Etf(null, "iShares Core MSCI World", "IE00B4L5Y983",
                             "IWDA", "USD", 0.20, 98.76,
                             "Accumulating", 78.40, 101.20, 62.3, "Physical","buy")
                     );
 
-            repository.save(new Etf(3L, "Xtrackers MSCI Emerging Markets", "IE00BTJRMP35",
+            repository.save(new Etf(null, "Xtrackers MSCI Emerging Markets", "IE00BTJRMP35",
                             "XMME", "EUR", 0.18, 24.31,
                             "Accumulating", 19.80, 26.10, 4.1, "Physical","hold")
                     );
 
-            repository.save(new Etf(4L, "iShares S&P 500", "IE0031442068",
+            repository.save(new Etf(null, "iShares S&P 500", "IE0031442068",
                             "IUSA", "USD", 0.07, 45.67,
                             "Accumulating", 36.20, 47.90, 89.7, "Physical","buy")
                     );
 
-            repository.save(new Etf(5L, "Amundi MSCI World", "LU1681043599",
+            repository.save(new Etf(null, "Amundi MSCI World", "LU1681043599",
                     "CW8", "EUR", 0.12, 412.30,
                     "Accumulating", 320.50, 425.80, 12.8, "Synthetic","sell")
             );
