224dc877d40eed65fe17e1a3df9795e86ed53724
NilsPO <nils.pohlers.np@gmail.com>
2026-05-25 01:51:57 +0200

    Add Service for WatchlistEntry

---
+++ src/main/java/de/dhbwravensburg/etfadvisor/service/WatchlistEntryService.java
@@ -0,0 +1,45 @@
+package de.dhbwravensburg.etfadvisor.service;
+
+import de.dhbwravensburg.etfadvisor.entity.Recommendation;
+import de.dhbwravensburg.etfadvisor.entity.WatchlistEntry;
+import de.dhbwravensburg.etfadvisor.repository.EtfRepository;
+import de.dhbwravensburg.etfadvisor.repository.WatchlistEntryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WatchlistEntryService {
+
+    private final WatchlistEntryRepository repository;
+    private final EtfRepository etfRepository;
+
+    public WatchlistEntryService (WatchlistEntryRepository repository, EtfRepository etfRepository){
+        this.repository = repository;
+        this.etfRepository=etfRepository;
+    }
+
+    public List<WatchlistEntry> findAll(){return this.repository.findAll(); }
+
+    public List<WatchlistEntry> findByEtfId(Long etfId){return this.repository.findByEtfId(etfId);}
+
+    public Optional<WatchlistEntry> create(Long etfId, WatchlistEntry entry){
+
+        return etfRepository.findById(etfId).map(etf -> {
+            entry.setEtf(etf);
+            return repository.save(entry);
+        });
+
+    }
+
+    public boolean delete(Long id){
+        if(!repository.existsById(id)){
+            return false;
+        }
+        repository.deleteById(id);
+        return true;
+    }
+
+
+}
