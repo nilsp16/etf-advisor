d30d5e5a3afeb4e220a49ea7aec1e0a340db90e3
NilsPO <nils.pohlers.np@gmail.com>
2026-06-05 02:02:48 +0200

    Add JOIN FETCH queries to prevent LazyInitializationException

--- src/main/java/de/dhbwravensburg/etfadvisor/repository/RecommendationRepository.java
+++ src/main/java/de/dhbwravensburg/etfadvisor/repository/RecommendationRepository.java
@@ -2,14 +2,17 @@ package de.dhbwravensburg.etfadvisor.repository;
 
 import de.dhbwravensburg.etfadvisor.entity.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
 
-    List<Recommendation> findByEtfId(Long etfId);
+    @Query("SELECT r FROM Recommendation r JOIN FETCH r.etf WHERE r.etf.id = :etfId")
+    List<Recommendation> findByEtfId(@Param("etfId") Long etfId);
 
-
-
+    @Query("SELECT r FROM Recommendation r JOIN FETCH r.etf")
+    List<Recommendation> findAllWithEtf();
 }
