96d75d74d3e231ae422dfdae289731b96127bf5e
NilsPO <nils.pohlers.np@gmail.com>
2026-06-11 12:43:57 +0200

    Add scheduled recommendation generation every 60 seconds

--- src/main/java/de/dhbwravensburg/etfadvisor/EtfAdvisorApplication.java
+++ src/main/java/de/dhbwravensburg/etfadvisor/EtfAdvisorApplication.java
@@ -2,8 +2,10 @@ package de.dhbwravensburg.etfadvisor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EtfAdvisorApplication {
 
     public static void main(String[] args) {
